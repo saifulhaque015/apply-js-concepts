e1724c9487ce7750bc423b6438aa58cecb5b474c
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-26 22:20:07 +0600

    conversion added

--- inch-feet.js
+++ inch-feet.js
@@ -1,3 +1,27 @@
-var inches = 132;
-var feet = inches / 12;
-console.log(feet);
+// function inchToFeet(inches) {
+//     var feet = inches / 12;
+//     return feet;
+// }
+
+// var myInches = 144;
+// var feet = inchToFeet(myInches);
+// console.log('my inches to feet', feet);
+
+// var dadiInches = 156;
+// var feet = inchToFeet(dadiInches);
+// console.log('dadi inches to feet is ', feet);
+
+// var naniInches = 176;
+// var feet = inchToFeet(naniInches);
+// console.log(feet.toFixed(2));
+
+// mile to kilo meter converter
+
+function mileToKilo(mile) {
+    var kilo = mile * 1.60;
+    return kilo;
+}
+
+var myMile = 5;
+var mile = mileToKilo(myMile);
+console.log(mile);
