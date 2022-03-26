f5e563bae3de843bddb9e477db290c878607bd12
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-26 22:38:42 +0600

    even odd done

--- even-odd.js
+++ even-odd.js
@@ -1,10 +1,12 @@
-// const number = 15;
-// if (number % 2 == 1) {
-//     console.log('odd number');
-// }
-// else {
-//     console.log("even number");
-// }
+const number = 15;
+if (number % 2 == 1) {
+    console.log('odd number');
+}
+else {
+    console.log("even number");
+}
+
+// even function
 
 function isEven(number) {
     if (number % 2 == 0) {
@@ -17,6 +19,19 @@ const myNumber = 1450;
 const isMyNumberEven = isEven(myNumber);
 console.log(isMyNumberEven);
 
-const herNumber = 5260;
+const herNumber = 5261;
 const isHerNumberEven = isEven(herNumber);
 console.log(isHerNumberEven);
+
+// odd function
+
+function isOdd(number) {
+    if (number % 2 == 1) {
+        return true;
+    }
+    return false;
+}
+
+const yourNumber = 11;
+const isYourNumberOdd = isOdd(yourNumber);
+console.log(isYourNumberOdd);
